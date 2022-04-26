4c1e54ea4438ebfb97d5462ab6df9f40c46f4eda
ogrom <ogromwang@gmail.com>
2022-04-26 09:54:36 +0800

    build: 修改 sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo building xiaohuazhu
 
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./cmd/main ./cmd/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o ./cmd/xhz ./cmd/main.go
 
 # 删除之前的镜像
 docker rmi -f xiaohuazhu
