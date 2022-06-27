5631af8afd81b6d0d48479b4ec940ac3fa057d1f
ogrom <ogromwang@gmail.com>
2022-06-27 18:26:29 +0800

    feat: 新增一个跨域

--- internal/server/http/router.go
+++ internal/server/http/router.go
@@ -15,6 +15,7 @@ func v1(r *gin.Engine, s *service.Service) {
 	// 8 Mib 这不能限制，是占用多少内存
 	r.MaxMultipartMemory = 8 << 20
 	routerGroup := r.Group("v1")
+	routerGroup.Use(Cors())
 
 	// 绕过鉴权
 	account := routerGroup.Group("account")
